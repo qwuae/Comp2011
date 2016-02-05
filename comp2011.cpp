a7c91a1097d8a1d3b283a0507cfac1c79e4ff0da
qwuae <qwuae@ust.hk>
2016-02-05 18:06:02 +0800

    2/5/16 initial of comp 2011

---
+++ comp2011.cpp
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+// delete?
+//============================================================================
+// Name        : COMP 2011.cpp
+// Language    : C++
+// Author      : qwuae@ust.hk
+// Version     : beta 1.0
+// Created	   : On 05/02/16 01:18
+// Copyright   : Copyright © 2016 qwuae@ust.hk. All rights reserved.
+// Description : C++ random testing programming.
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << 4+5
+    return 0
+}
