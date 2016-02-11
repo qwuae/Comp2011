dbdbd4ea66dade1161d58078feb5709d3a035ddf
qwuae <qwuae@ust.hk>
2016-02-11 14:24:17 +0800

    1

--- lab0.cpp
+++ lab0.cpp
@@ -1,13 +1,11 @@
-//#include <stdio.h>
-// delete?
 //============================================================================
-// Name        : COMP 2011.cpp
+// Name        : lab0.cpp
 // Language    : C++
 // Author      : qwuae@ust.hk
 // Version     : beta 1.0
-// Created	   : On 05/02/16 01:18
+// Created	   : On 10/02/16 01:18
 // Copyright   : Copyright © 2016 qwuae@ust.hk. All rights reserved.
-// Description : C++ random testing programming.
+// Description : C++ lab 0 EXERCISE programming.
 //============================================================================
 
 
@@ -17,11 +15,13 @@ using namespace std;
 
 int main()
 {
-   int x, y;
-if (x=9)
-{
-	y=3;
-}
-cout<<y;
+   int x, y, z,p;
+
+   x = 5;
+   y = 6;
+   z = x+y;
+   p = x*y;
+   cout << "The sum of " << x << " and " << y << " is " << z << endl;
+   cout << "The product of " << x << " and " << y << " is " << p << endl;
    return 0;
 }
