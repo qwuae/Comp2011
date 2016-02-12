86f1a3c9a0b2f52e5a125f7ab6584f9c0ab74a56
qwuae <qwuae@ust.hk>
2016-02-12 21:36:44 +0800

    endl bug???\n

--- lab1.cpp
+++ lab1.cpp
@@ -11,29 +11,29 @@
 using namespace std;
 int main()
 {
-    int hour, minute, duration; // Define 3 variables that will hold the time
+    int hour, minute, duration; // Define 3 variables that will define the time
+    cout << "Dear master please create an event for our future"<<endl;
     do
     {
-        cout << "Dear master please create an event for our future";
-        cout << "Please enter the starting hour (0 - 23)";
+        cout << "Please enter the starting hour (0 - 23)"<<endl;
         cin>>hour;
-        cout << "Please enter the starting minutes (0 - 59)";
+        cout << "Please enter the starting minutes (0 - 59)"<<endl;
         cin>>minute;
-        do
-        {
-            cout << "Please enter the duration (hours)";
-            cin>>duration;
-            if (duration<=0) {
-                cout << "Master,duration cannot be zero or negative";
-            }
-        } while (duration<=0);
-        
         if (hour<0||hour>23||minute>59||minute<0)
         {
-            cout << "Master,there is error in the input, please tell me again";
+            cout << "Master,there is error in the input, please tell me again"<<endl;
         }
     } 
     while (hour<0||hour>23||minute>59||minute<0);
+    do
+    {
+        cout << "Please enter the duration (hours)"<<endl;
+        cin>>duration;
+        if (duration<=0) {
+            cout << "Master,duration cannot be zero or negative"<<endl;
+        }
+    } while (duration<=0);
+    
     cout <<"My master,The event ends at "<< (hour+duration)%24<<":"<<minute<<endl;
     cout << (hour+duration)/24<<" days later"<<endl;
     return 0;
